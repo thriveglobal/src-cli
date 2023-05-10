0f6994e704e9f8e3905cf3d861e1a74f04900b51
Jason Hawk Harris <jason.harris@sourcegraph.com>
2023-05-10 16:54:17 -0500

    add routing for pod and container options

--- internal/scout/usage/usage.go
+++ internal/scout/usage/usage.go
@@ -12,12 +12,12 @@ import (
 type Option = func(config *Config)
 
 type Config struct {
-    namespace string
-    pod string
-    container string
-    docker bool
-    k8sClient *kubernetes.Clientset
-    dockerClient *client.Client
+	namespace    string
+	pod          string
+	container    string
+	docker       bool
+	k8sClient    *kubernetes.Clientset
+	dockerClient *client.Client
 }
 
 func WithNamespace(namespace string) Option {
@@ -39,26 +39,36 @@ func WithContainer(container string) Option {
 }
 
 func K8s(ctx context.Context, clientSet *kubernetes.Clientset, restConfig *rest.Config, opts ...Option) error {
-    cfg := &Config{
-        namespace: "default",
-        pod: "",
-        container: "", 
-        docker: false,
-        k8sClient: clientSet,
-        dockerClient: nil,
-    }
+	cfg := &Config{
+		namespace:    "default",
+		pod:          "",
+		container:    "",
+		docker:       false,
+		k8sClient:    clientSet,
+		dockerClient: nil,
+	}
 
-    for _, opt := range opts {
-        opt(cfg)
-    }
+	for _, opt := range opts {
+		opt(cfg)
+	}
 
-    return listResourceUsage(ctx, cfg)
+	return listResourceUsage(ctx, cfg)
 }
 
 func listResourceUsage(ctx context.Context, cfg *Config) error {
-    // code for listing resource usage
-    // plug into prom API?
-    fmt.Println("listing resource usage works now.")
-    return nil
+	// code for listing resource usage
+	// plug into prom API?
+	if cfg.pod != "" {
+		fmt.Printf("listing resource usage with POD (%s) works now.", cfg.pod)
+		return nil
+	}
+    
+	if cfg.container != "" {
+		fmt.Printf("listing resource usage with CONTAINER (%s) works now.", cfg.container)
+		return nil
+	}
+	// code for listing resource usage
+	// plug into prom API?
+	fmt.Println("listing resource usage works now.")
+	return nil
 }
-
